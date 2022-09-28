a25e685ded9372e12675af8d54e283b59d9b97b6
jvitormcs <vitormouracs@gmail.com>
2022-09-28 19:39:51 -0300

    ajuste api

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 
 const api = axios.create({
-    baseURL: 'http://54.233.223.26:3030/'
+    baseURL: 'https://54.233.223.26:3030/'
    /*  baseURL: 'http://26.75.88.228:3030/' */
 })
 
