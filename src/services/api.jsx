f37ea1a370104fe85318d2335b91855ee221de96
jvitormcs <vitormouracs@gmail.com>
2022-09-28 21:23:17 -0300

    ajustes

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -2,7 +2,8 @@ import axios from 'axios'
 
 
 const api = axios.create({
-    baseURL: 'http://54.233.223.26:3030/'
+    baseURL: 'https://lm-participacoes.herokuapp.com/'
+    /* baseURL: 'http://54.233.223.26:3030/' */
    /*  baseURL: 'http://26.75.88.228:3030/' */
 })
 
